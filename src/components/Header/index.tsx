f3fb75381e730b5019ee9c361c8c8621155baa6c
MarcosFelintoLN <marcos.leao@escolar.ifrn.edu.br>
2025-12-03 15:12:26 -0300

    style: criação do componente header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,25 @@
+import Logo from "../../assets/logo-if.png";
+import Perfil from "../../assets/perfil.png";
+import styles from "./Header.module.css";
+
+export default function Header() {
+    return (
+        <header className={styles.header}>
+            <div className={styles.left}>
+                <img className={styles.logo} src={Logo} alt="Logo do IF" />
+                <span className={styles.title}> SADT </span>
+            </div>
+
+            <div className={styles.right}>
+                <nav className={styles.nav}>
+                    <a href="/turmas">Minhas Turmas</a>
+                    <a href="/classificacoes">Classificações</a>
+                </nav>
+
+                <div className={styles.profile}>
+                    <img className={styles.avatar} src={Perfil} alt="User" />
+                </div>
+            </div>
+        </header>
+    );
+}
