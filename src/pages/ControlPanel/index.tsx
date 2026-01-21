13f0615ab138d6603dd7f933f1fef6e54fa8d017
caarlosriian <rlina1785@gmail.com>
2026-01-20 23:05:15 -0300

    style: Implementação inicial antes da criação de componente na tela do admin

--- src/pages/ControlPanel/index.tsx
+++ src/pages/ControlPanel/index.tsx
@@ -5,20 +5,42 @@ import Footer from "../../components/Footer";
 import Pagination from "../../components/Pagination";
 import styles from "./ControlPanel.module.css";
 import SadtIcon from "../../assets/logo-if.png";
+import TooltipIcon from "../../assets/tooltip-icon.png";
+import AlertIcon from "../../assets/alert-icon-message.png";
 
 const ITEMS_PER_PAGE = 8;
-
-const alunos = Array.from({ length: 201 }).map((_, index) => ({
-  nome: `Aluno ${index + 1}`,
-  ira: "61,22", // IRA fixo para todos
-  matricula: "20241094040001", // matrícula fixa
-  curso: "Informática",
-}));
 
 export default function ControlPanel() {
   const [activeTab, setActiveTab] = useState<
     "alunos" | "professores" | "turmas" | "cursos"
   >("alunos");
+
+  const alunos = Array.from({ length: 201 }).map((_, index) => ({
+  nome: `Aluno ${index + 1}`,
+  ira: "61,22", 
+  matricula: "20241094040001", 
+  curso: "Informática",
+}));
+
+type Aluno = {
+  nome: string;
+  ira: string;
+  matricula: string;
+  curso: string;
+};
+
+const cursosDisponiveisTeste = [
+  "Informática",
+  "Apicultura",
+  "Alimentos",
+  "Analise e Desenvolvimento de Sistemas",
+  "Química",
+  "Agroindustria"
+];
+
+const [isModalOpen, setIsModalOpen] = useState(false);
+const [selectedAluno, setSelectedAluno] = useState<Aluno | null>(null);
+const [isDirty, setIsDirty] = useState(false);
 
   const [currentPage, setCurrentPage] = useState(1);
 
@@ -45,7 +67,7 @@ export default function ControlPanel() {
 
   const handleDownloadModelo = () => {
     const link = document.createElement("a");
-    link.href = "/path/to/modelo.xlsx"; // Atualize para o caminho real
+    link.href = "/path/to/modelo.xlsx"; 
     link.download = "modelo.xlsx";
     link.click();
   };
@@ -180,28 +202,49 @@ export default function ControlPanel() {
                       </div>
                     </div>
 
-                    <button className={styles.editButton}>Editar</button>
+                    <button
+                        className={styles.editButton}
+                        onClick={() => {
+                          setSelectedAluno(aluno);
+                          setIsModalOpen(true);
+                          setIsDirty(false);
+                        }}
+                      >
+                        Editar
+                    </button>
                   </div>
                 ))}
               </div>
               
 
-              <div className={styles.paginationWrapper}>
-                <Pagination
-                  currentPage={currentPage}
-                  totalPages={totalPages}
-                  onPageChange={setCurrentPage}
+          <div className={styles.paginationWrapper}>
+            <div className={styles.downloadWrapper}>
+              <button
+                className={styles.downloadButton}
+                onClick={handleDownloadModelo}
+              >
+                Baixar modelo
+              </button>
+
+              <div className={styles.tooltip}>
+                <img
+                  className={styles.tooltip_icon}
+                  src={TooltipIcon}
+                  alt="Ícone"
                 />
-
-                <button
-                  className={styles.downloadButton}
-                  onClick={handleDownloadModelo}
-                >
-                  Baixar modelo
-                </button>
-
-                
+                <span className={styles.tooltip_text}>
+                  Baixe o modelo da planilha para preencher e importar os dados.
+                </span>
               </div>
+            </div>
+
+            <Pagination
+              currentPage={currentPage}
+              totalPages={totalPages}
+              onPageChange={setCurrentPage}
+            />
+          </div>
+
             </div>
           )}
 
@@ -223,7 +266,117 @@ export default function ControlPanel() {
         </div>
       </main>
 
-      <Footer />
+      {isModalOpen && selectedAluno && (
+  <div className={styles.modalOverlay}>
+    <div className={styles.modal}>
+      <h2 className={styles.h2_edit_modal}>Editar Aluno</h2>
+
+      {isDirty && (
+        <div className={styles.alertWarning}>
+          <img className={styles.alert_icon} src={AlertIcon} alt="icon-alert-message" />
+          <h2 className={styles.h2_alert_message}>Existem alterações não salvas.</h2>
+        </div>
+      )}
+
+      <form
+        onChange={() => setIsDirty(true)}
+        onSubmit={(e) => {
+          e.preventDefault();
+
+          const iraValue = parseFloat(selectedAluno.ira.replace(",", "."));
+
+          if (iraValue < 0.01 || iraValue > 99.99 || isNaN(iraValue)) {
+            alert("IRA deve estar entre 0,01 e 99,99");
+            return;
+          }
+
+          setIsModalOpen(false);
+        }}
+      >
+        <label>
+          Aluno
+          <input
+            type="text"
+            value={selectedAluno.nome}
+            onChange={(e) =>
+              setSelectedAluno({ ...selectedAluno, nome: e.target.value })
+            }
+          />
+        </label>
+
+        <label>
+          IRA
+          <input
+            type="number"
+            step="0.01"
+            min="0.01"
+            max="99.99"
+            value={selectedAluno.ira}
+            onChange={(e) =>
+              setSelectedAluno({ ...selectedAluno, ira: e.target.value })
+            }
+          />
+        </label>
+
+        <label>
+          Matrícula
+          <input
+            type="text"
+            value={selectedAluno.matricula}
+            onChange={(e) =>
+              setSelectedAluno({
+                ...selectedAluno,
+                matricula: e.target.value,
+              })
+            }
+          />
+        </label>
+
+        <label>
+  Curso
+  <div className={styles.selectWrapper}>
+    <input
+      readOnly
+      className={styles.customSelect}
+      value={selectedAluno.curso}
+      placeholder="-- Selecione um curso --"
+    />
+    <div className={styles.optionsList}>
+      {cursosDisponiveisTeste.map((curso) => (
+        <div
+          key={curso}
+          className={styles.optionItem}
+          onClick={() => {
+            setSelectedAluno({ ...selectedAluno, curso: curso });
+            setIsDirty(true);
+          }}
+        >
+          {curso}
+        </div>
+      ))}
     </div>
+  </div>
+</label>
+
+        <div className={styles.modalActions}>
+          <button
+            type="button"
+            className={styles.cancelButton}
+            onClick={() => setIsModalOpen(false)}
+          >
+            Cancelar
+          </button>
+
+          <button type="submit" className={styles.saveButton}>
+            Salvar
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+)}
+
+      <Footer />
+    </div> 
   );
 }
